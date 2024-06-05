ad3121390f0e6c96a4ac292678c355cfc5a481aa
Kushin-Oleg <kushin250905@gmail.com>
2024-06-05 23:57:58 +0300

    базовые изменения

---
+++ attemp server on C++/toolbox.h
@@ -0,0 +1,17 @@
+#ifndef SERVER_TOOLBOX_H
+#define SERVER_TOOLBOX_H
+#endif //SERVER_TOOLBOX_H
+
+#include "httplib.h"
+
+
+
+class serv{
+public:
+
+    //функция для отправки пост запросов
+    void post_output() {
+
+    }
+
+};
--- server/toolbox.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVER_TOOLBOX_H
-#define SERVER_TOOLBOX_H
-
-#endif //SERVER_TOOLBOX_H
-
-class serv{
-public:
-    
-};
