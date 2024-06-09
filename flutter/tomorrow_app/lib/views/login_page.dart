e173719eb00d30fe8b9beac542499853f880a7c5
ANDr <andrkrsv@gmail.com>
2024-06-09 18:40:32 +0300

    заготовка

--- flutter/tomorrow_app/lib/views/login_page.dart
+++ flutter/tomorrow_app/lib/views/login_page.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          //  сюда логотипы да
+          Icon(
+            Icons.lock,
+          )
+          //текст
+
+          //юзер
+
+          // пароль
+
+          //восстановление
+
+          //войти
+
+          // продолжить с.....
+
+          //сервисы для логина
+
+          //зарегатся
+        ],
+      ),
+    );
+  }
+}
