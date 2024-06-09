95238138c9adf2c9281afeb155260d89dec49194
andrkrsv <75070600+andrkrsv@users.noreply.github.com>
2024-06-09 18:22:58 +0300

    Delete flutter directory

--- flutter/tomorrow_app/lib/view_models/app_view_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../models/task_models.dart';
-import '../models/user_model.dart';
-
-class AppViewModel extends ChangeNotifier {
-  List<Task> tasks = <Task>[];
-  User user = User("Gay Power");
-}
